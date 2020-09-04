5486b3e7e6267df630d93db421093b417f55ad8c
Matthew Nevle <manevle16@gmail.com>
2020-09-03 20:40:23 -0500

    setup package for server

--- website-server/handler.js
+++ website-server/handler.js
@@ -22,12 +22,9 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // unifies the get and post requests. Post request has body as parameter, it is converted to query same as get request.
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   if (Object.keys(req.query).length === 0 && req.query.constructor === Object) {
     req.query = req.body;
   }
@@ -36,14 +33,14 @@ app.use(function(req, res, next) {
 
 app.use('/users', userRouter());
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 let httpsServer = https.createServer(options, app);
 let httpServer = http.createServer(app);
 
-httpsServer.listen(port, function() {
+httpsServer.listen(port, function () {
   console.log('Running  on port ' + port);
 });
 
