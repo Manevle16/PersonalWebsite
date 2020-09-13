1142227a4b6d2e0617465446c83548f0f269069a
Matthew Nevle <manevle16@gmail.com>
2020-09-13 16:41:00 -0500

    added google analytics

--- my-website/src/index.jsx
+++ my-website/src/index.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Website from './Website.jsx';
+import ReactGA from 'react-ga';
+
+//Add google analytics
+ReactGA.initialize('UA-177838466-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 document.body.style.backgroundColor = '#FFD2BC';
 ReactDOM.render(<Website />, document.getElementById('root'));
